cf1285683599402b09fa467a8ba44c37e7e18b8c
elino <elinor.g@campus.technion.ac.il>
2021-12-03 11:48:13 +0200

    Finished stage #1 of debug session - passed staff tests

--- dflow_calc-skeleton.cpp
+++ dflow_calc-skeleton.cpp
@@ -14,23 +14,25 @@ using namespace std;
 const int DST = (0);
 const int SRC1 = (1);
 const int SRC2 = (2);
-const int ENTRY_ORDINAL_NUMBER = (-1);
 const int SUCCESS = (0);
 const int FAILURE = (-1);
 
-
-unsigned int EXIT_ORDINAL_NUMBER = -2;
+int ENTRY_ORDINAL_NUMBER = -1;
+int EXIT_ORDINAL_NUMBER = -2;
 
 struct Node {
-    int instruction_ordinal_number = -1;
+    int instruction_ordinal_number = ENTRY_ORDINAL_NUMBER;
     unsigned int opcode = -1;
+    int instruction_latency = 0;
     int dst_index = -1;
-    unsigned int src1_index = -1;
-    unsigned int src2_index = -1;
+    unsigned int src1_index = ENTRY_ORDINAL_NUMBER;
+    unsigned int src2_index = ENTRY_ORDINAL_NUMBER;
+    int depth = 0;
 
-    void _add_information(int ordinal_number, const InstInfo instruction_info) {
+    void _add_information(int ordinal_number, const InstInfo instruction_info, const unsigned int opsLatency[]) {
         instruction_ordinal_number = ordinal_number;
         opcode = instruction_info.opcode;
+        instruction_latency = (opcode != -1) ? (int)opsLatency[opcode] : 0;
         dst_index = instruction_info.dstIdx;
         src1_index = instruction_info.src1Idx;
         src2_index = instruction_info.src2Idx;
@@ -41,7 +43,7 @@ typedef vector<vector<Node>> InstructionMatrix;
 
 struct Register {
     bool is_used = false;
-    int master_inst_ordinal_number = -1;
+    int master_inst_ordinal_number = ENTRY_ORDINAL_NUMBER;
 
     void _mark_used(int inst_ordinal_num) {
         is_used = true;
@@ -92,7 +94,8 @@ struct DataFlowGraph {
         used_registers.resize(NUMBER_OF_REGISTERS);
         exit_dependencies_array = new Node[numOfInsts];
 
-        EXIT_ORDINAL_NUMBER = eff_number_of_instructions;
+        ENTRY_ORDINAL_NUMBER = numOfInsts;
+        EXIT_ORDINAL_NUMBER = numOfInsts + 1;
 
         _init_data_flow_graph(opsLatency);
 
@@ -104,13 +107,13 @@ struct DataFlowGraph {
         delete[] opcodes_latencies;
     }
 
-    void _reset_fields(InstInfo inst_info) {
+    void _reset_fields(InstInfo *inst_info) {
         /*** Helper Function to reset all instruction info struct fields. used in _add_dependencies
          *   in case the instruction depends only on Entry node. ***/
-        inst_info.opcode = -1;
-        inst_info.dstIdx = -1;
-        inst_info.src1Idx = -1;
-        inst_info.src2Idx = -1;
+        inst_info->opcode = -1;
+        inst_info->dstIdx = -1;
+        inst_info->src1Idx = ENTRY_ORDINAL_NUMBER;
+        inst_info->src2Idx = ENTRY_ORDINAL_NUMBER;
     }
 
     void _get_instruction_info_by_id(int id, InstInfo *info) {
@@ -127,12 +130,14 @@ struct DataFlowGraph {
          *   it's ordinal number and ignore it. use the pointed instructions array to find which instr. Exit should
          *   point at. ***/
 
+        int counter = 0;
         for (int id = 0; id < eff_number_of_instructions - 2; ++id) {
             if(pointed_instructions[id] == 0) {
                 // no one points to this instruction
                 InstInfo info;
                 _get_instruction_info_by_id(id, &info);
-                exit_dependencies_array[id]._add_information(id,info);
+                exit_dependencies_array[counter]._add_information(id,info, opcodes_latencies);
+                counter++;
             }
         }
     }
@@ -142,49 +147,58 @@ struct DataFlowGraph {
         /*** update sources dependency. if srcX was not written by previous instruction (i.e., no RAW)
              need to point to Entry node. if we're an exit node we need to point to all un-pointed instructions ***/
 
-        // if it's an entry node no need to update
-        if(inst_ordinal_number == ENTRY_ORDINAL_NUMBER) return;
+        InstInfo src1_inst_info, src2_inst_info;
+        int src1_ordinal_number = ENTRY_ORDINAL_NUMBER, src2_ordinal_number = ENTRY_ORDINAL_NUMBER;
+        // in case there's no RAW, and srcX wasn't written to by previous instruction, point to Entry node.
+        _reset_fields(&src1_inst_info);
+        _reset_fields(&src2_inst_info);
 
-        else if(inst_ordinal_number == EXIT_ORDINAL_NUMBER) {
-            _add_exit_node_dependencies();
-            return;
+        // check if there's a dependency and upon which inst. for each one of the srcs
+        if(used_registers[progTrace[inst_ordinal_number].src1Idx].is_used) {
+            src1_ordinal_number = used_registers[progTrace[inst_ordinal_number].src1Idx].master_inst_ordinal_number;
+
+            src1_inst_info = progTrace[src1_ordinal_number];
+            pointed_instructions[src1_ordinal_number] = 1;
+
+        }
+        if(used_registers[progTrace[inst_ordinal_number].src2Idx].is_used) {
+            src2_ordinal_number = used_registers[progTrace[inst_ordinal_number].src2Idx].master_inst_ordinal_number;
+
+            src2_inst_info = progTrace[src2_ordinal_number];
+            pointed_instructions[src2_ordinal_number] = 1;
         }
 
-        else {
-            InstInfo src1_inst_info, src2_inst_info;
-            int src1_ordinal_number, src2_ordinal_number;
-            // in case there's no RAW, and srcX wasn't written to by previous instruction, point to Entry node.
-            _reset_fields(src1_inst_info);
-            _reset_fields(src2_inst_info);
-
-            // check if there's a dependency and upon which inst. for each one of the srcs
-            if(used_registers[progTrace[inst_ordinal_number].src1Idx].is_used) {
-                src1_ordinal_number = used_registers[progTrace[inst_ordinal_number].src1Idx].master_inst_ordinal_number;
-
-                src1_inst_info = progTrace[src1_ordinal_number];
-                pointed_instructions[src1_ordinal_number] = 1;
-
-            }
-            if(used_registers[progTrace[inst_ordinal_number].src2Idx].is_used) {
-                src2_ordinal_number = used_registers[progTrace[inst_ordinal_number].src2Idx].master_inst_ordinal_number;
-
-                src2_inst_info = progTrace[src2_ordinal_number];
-                pointed_instructions[src2_ordinal_number] = 1;
-            }
-
-            // update node's dependencies.
-            instruction_vertices[inst_ordinal_number][SRC1]._add_information(inst_ordinal_number, src1_inst_info);
-            instruction_vertices[inst_ordinal_number][SRC2]._add_information(inst_ordinal_number, src2_inst_info);
-        }
+        // update node's dependencies.
+        instruction_vertices[inst_ordinal_number][SRC1]._add_information(src1_ordinal_number, src1_inst_info, opcodes_latencies);
+        instruction_vertices[inst_ordinal_number][SRC2]._add_information(src2_ordinal_number, src2_inst_info, opcodes_latencies);
     }
 
+    int _get_latency(int i) {
+        return instruction_vertices[i][DST].depth + instruction_vertices[i][DST].instruction_latency;
+    }
+
+    int _calc_depth(int instruction_ordinal_number) {
+        if (instruction_ordinal_number == ENTRY_ORDINAL_NUMBER)
+            return 0;
+
+        int dep_a_depth = _get_latency(instruction_vertices[instruction_ordinal_number][SRC1].instruction_ordinal_number);
+        int dep_b_depth = _get_latency(instruction_vertices[instruction_ordinal_number][SRC2].instruction_ordinal_number);
+        instruction_vertices[instruction_ordinal_number][DST].depth = max(dep_a_depth, dep_b_depth);
+        return instruction_vertices[instruction_ordinal_number][DST].depth;
+    }
+
+    int _update_intrcutions_depths() {
+        for (int i = 0; i < eff_number_of_instructions - 2; ++i) {
+            _calc_depth(i);
+        }
+    }
 
     void _add_vertex(int instruction_ordinal_number, const InstInfo progTrace[]) {
         /*** Function to update specific instruction by it's ordinal number.
          *   for the i'th instruction we will update the adequate node in the instruction_vertices vector
          *   and update the adjacency list holding it's dependencies. ***/
 
-        instruction_vertices[instruction_ordinal_number][DST]._add_information(instruction_ordinal_number, progTrace[instruction_ordinal_number]);
+        instruction_vertices[instruction_ordinal_number][DST]._add_information(instruction_ordinal_number, progTrace[instruction_ordinal_number], opcodes_latencies);
 
         _add_dependencies(instruction_ordinal_number, progTrace);
     }
@@ -196,7 +210,7 @@ struct DataFlowGraph {
 
         int used_register_number;
 
-        for (int inst_ordinal_num = 0; inst_ordinal_num < eff_number_of_instructions; ++inst_ordinal_num) {
+        for (int inst_ordinal_num = 0; inst_ordinal_num < eff_number_of_instructions - 2; ++inst_ordinal_num) {
 
             // assume this function will also add the dependencies
             _add_vertex(inst_ordinal_num, progTrace);
@@ -204,30 +218,35 @@ struct DataFlowGraph {
             used_register_number = progTrace[inst_ordinal_num].dstIdx;
             used_registers[used_register_number]._mark_used(inst_ordinal_num);
         }
+
+        _update_intrcutions_depths();
+
+        _add_exit_node_dependencies();
+
+        instruction_vertices[ENTRY_ORDINAL_NUMBER][DST].instruction_ordinal_number = ENTRY_ORDINAL_NUMBER;
+        instruction_vertices[EXIT_ORDINAL_NUMBER][DST].instruction_ordinal_number = EXIT_ORDINAL_NUMBER;
     }
 
-    int getGraphDiameter() {
 
+    int getProgDepthRec(){
+        int idx = 0, max_depth = 0, curr_depth = 0, latency = 0;
+        while (exit_dependencies_array[idx].instruction_ordinal_number != -1) {
+            curr_depth = instruction_vertices[exit_dependencies_array[idx].instruction_ordinal_number][DST].depth +
+                         instruction_vertices[exit_dependencies_array[idx].instruction_ordinal_number][DST].instruction_latency;
+            if(curr_depth > max_depth) {
+                max_depth = curr_depth;
+            }
+            idx++;
+        }
+        return max_depth;
     }
 
-    int getInstDepthRec(unsigned int src_ordinal_number, unsigned int latency) {
-
-        if(src_ordinal_number == -1) return latency;
-
-        // get src dependencies
-        Node dep1_node = instruction_vertices[src_ordinal_number][SRC1];
-        Node dep2_node = instruction_vertices[src_ordinal_number][SRC2];
-
-        latency += opcodes_latencies[dep1_node.opcode];
-        latency += opcodes_latencies[dep2_node.opcode];
-
-        getInstDepthRec(dep1_node.instruction_ordinal_number, latency);
-        getInstDepthRec(dep2_node.instruction_ordinal_number, latency);
-    }
 
     int getInstDeps(unsigned int theInst, int *src1DepInst, int *src2DepInst) {
         *src1DepInst = instruction_vertices[theInst][SRC1].instruction_ordinal_number;
+        if(*src1DepInst == ENTRY_ORDINAL_NUMBER) *src1DepInst = -1;
         *src2DepInst = instruction_vertices[theInst][SRC2].instruction_ordinal_number;
+        if(*src2DepInst == ENTRY_ORDINAL_NUMBER) *src2DepInst = -1;
         return SUCCESS;
     }
 };
@@ -258,7 +277,7 @@ int getInstDepth(ProgCtx ctx, unsigned int theInst) {
     if (theInst < 0 || theInst >= (dflow->eff_number_of_instructions-2)) {
         return FAILURE;
     }
-    return dflow->getInstDepthRec(theInst, latency);
+    return dflow->instruction_vertices[theInst][DST].depth;
 }
 
 int getInstDeps(ProgCtx ctx, unsigned int theInst, int *src1DepInst, int *src2DepInst) {
@@ -274,9 +293,10 @@ int getInstDeps(ProgCtx ctx, unsigned int theInst, int *src1DepInst, int *src2De
 
 int getProgDepth(ProgCtx ctx) {
     if(ctx == nullptr) return -1;
-    int latency = 0;
     DataFlowGraph *dflow = (DataFlowGraph*)ctx;
-    return dflow->getInstDepthRec(-1, latency);
+
+    // pass the Exit node ordinal number as the source ordinal number from which to start calculating the path.
+    return dflow->getProgDepthRec();
 }
 
 
